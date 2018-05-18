0b1c03b2e5a410edd4b9377b391a9857d48df1b7
Fernando Jorge Mota <f.j.mota@hotmail.com>
2018-05-18 21:22:23 +0000

    first commit

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+const uppercase = /[A-Z]/g;
+
+function formatChar(char) {
+    return "-"+char.toLowerCase();
+}
+
+export function formatString(str) {
+    return str.replace(str, formatChar);
+}
+
+export function processAttributes(attrs) {
+    let result = {};
+    for(let key of Object.keys(attrs)) {
+        let newKey = formatString(key);
+        result[newKey] = attrs[key];
+    }
+    return result;
+}
