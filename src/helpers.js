9c52df11cb29e3cc751bbcaedf9eac1a00db11a9
Fernando Jorge Mota <f.j.mota13@gmail.com>
2018-06-30 19:37:55 -0300

    Refactor components to not use processAttributes and allow the class to be managed at will

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,4 @@
 const regex = /%s/gi;
-export function formatString(str) {
-    return str.replace("_", "-");
-}
-
-export function processAttributes(attrs) {
-    let result = {};
-    for(let key of Object.keys(attrs)) {
-        let newKey = formatString(key);
-        result[newKey] = attrs[key];
-    }
-    return result;
-}
 
 export function processClasses(classes, userClasses) {
     if (Array.isArray(userClasses)) {
