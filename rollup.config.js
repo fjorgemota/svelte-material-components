fba5340de625c6bf05fd12e95554c6d093582696
Fernando Jorge Mota <f.j.mota13@gmail.com>
2018-07-21 21:18:00 -0300

    Update rollup config file to maintain svelte shared functions on a separate module, with support for UMD and ES module imports

--- rollup.config.js
+++ rollup.config.js
@@ -5,16 +5,47 @@ const fs  = require('fs');
 var names = fs.readdirSync('./src/').filter((name) => name.split('.')[1] == 'html').map((name) => name.split('.')[0]);
 let imports = [];
 var config = [];
+let pathES = {};
+let pathUMD = {};
+let svelteShared = "svelte/shared.js";
+let svelteSharedComputed = require.resolve(svelteShared);
+pathES[svelteSharedComputed] = "./shared.js";
+pathUMD[svelteSharedComputed] = "./shared.umd.js";
+var globals = {};
+globals[svelteSharedComputed] = "svelteShared";
+config.push({
+    input: svelteSharedComputed,
+    output: [
+        { file: 'dist/shared.js', 'format': 'es'}, 
+        { file: 'dist/shared.umd.js', 'format': 'umd', 'name': "svelteShared"}
+    ]
+})
 for(let name of names) {
     config.push({
 		input: 'src/'+name+'.html',
 		output: [
-			{ file: 'dist/'+name+'.js', 'format': 'es' },
-			{ file: 'dist/'+name+'.umd.js', 'format': 'umd', name: name }
+			{ file: 'dist/'+name+'.js', 'format': 'es', paths: pathES }
 		],
+        external: [
+            svelteSharedComputed,
+        ],
 		plugins: [
 			resolve(),
-            commonjs(),
+	        commonjs(),
+			svelte()
+		]
+	});
+    config.push({
+		input: 'src/'+name+'.html',
+		output: [
+            { file: 'dist/'+name+'.umd.js', 'format': 'umd', name: name, paths: pathUMD, globals: globals }
+		],
+        external: [
+            svelteSharedComputed,
+        ],
+		plugins: [
+			resolve(),
+	        commonjs(),
 			svelte()
 		]
 	});
