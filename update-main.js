fe7005a29b5187b7291167bac1388adf385d667b
Fernando Jorge Mota <f.j.mota13@gmail.com>
2018-07-23 04:34:26 -0300

    Remove duplicated exports

--- update-main.js
+++ update-main.js
@@ -6,15 +6,15 @@ if (process.argv.length > 2 && process.argv.pop() === "dist") {
     directory = "dist/";
 }
 var getComponentName = (name) => name.split(".")[0];
-var isComponent = (name) => name[0] === name[0].toUpperCase() && name.split(".").length === 2;
-var getComponentAndFileName = (name) => {return {filename: name, componentName: getComponentName(name)}};
+var isComponent = (name) => name[0] === name[0].toUpperCase() && name.split(".").length === 2 && name.split(".")[1] === "js";
+var getComponentAndFileName = (name) => {return {nodeFilename: name, esFilename: name.replace(".js", ".mjs"), componentName: getComponentName(name)}};
 
 fs.readdirSync(directory)
     .filter(isComponent)
     .map(getComponentAndFileName)
-    .forEach(({filename, componentName}) => {
-        contentNode.push(`module.exports.${componentName} = require('./${filename}');`);
-        contentModule.push(`export {default as ${componentName}} from './${filename}';`);
+    .forEach(({nodeFilename, esFilename, componentName}) => {
+        contentNode.push(`module.exports.${componentName} = require('./${nodeFilename}');`);
+        contentModule.push(`export {default as ${componentName}} from './${esFilename}';`);
     });
 fs.writeFileSync(directory+"index.js", contentNode.join("\n"));
 fs.writeFileSync(directory+"index.mjs", contentModule.join("\n"));
